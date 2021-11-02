0f22ca7cf311cd4b20573d29a06615f15b1b7041
chaukhoa97 <jonhkane00@gmail.com>
2021-11-02 16:49:10 +0700

    a

--- src/Page/AccountPage.jsx
+++ src/Page/AccountPage.jsx
@@ -1,4 +1,4 @@
-import { Layout, Form, Input } from 'antd';
+import { Layout, Form, Input, Button, Space } from 'antd';
 import MainLayout from '../Layout/MainLayout';
 import { useForm, Controller, useController } from 'react-hook-form';
 import { Route, Switch, Link } from 'react-router-dom';
@@ -9,7 +9,13 @@ function AccountPage() {
   return (
     <MainLayout>
       <Layout.Content>
-        <SignIn></SignIn>
+        <div className="mx-auto mt-5 account__form rounded-3">
+          <div className="d-flex justify-content-center">
+            <h2 className="active">Đăng nhập</h2>
+            <h2>Đăng ký</h2>
+          </div>
+          <SignIn></SignIn>
+        </div>
         <Link to="/account/sign-up">Sign Up</Link>
       </Layout.Content>
     </MainLayout>
@@ -18,45 +24,58 @@ function AccountPage() {
 
 function SignIn() {
   const {
-    register,
     handleSubmit,
     formState: { errors },
     watch,
     control,
   } = useForm();
-  const { field } = useController({ name: 'email' });
 
   const onSubmit = (data) => console.log(data);
   console.log(watch(['email', 'password']));
   return (
-    <Layout.Content>
-      <form onSubmit={handleSubmit(onSubmit)}>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <Space direction="vertical">
         <Controller
           name="email"
           control={control}
+          rules={{ required: true }}
           render={({ field }) => (
             <Input
               {...field}
+              size="large"
               prefix={<FontAwesomeIcon icon="fa-solid fa-user" />}
-              placeholder="bruh"
+              placeholder="Email"
             />
           )}
         />
-        {errors.email && <span>This field is required</span>}
+        {errors.email && (
+          <span className="account__error">Hãy nhập email của bạn!</span>
+        )}
         <Controller
           name="password"
           control={control}
+          rules={{ required: true }}
           render={({ field }) => (
             <Input
               {...field}
+              size="large"
               prefix={<FontAwesomeIcon icon="fa-solid fa-lock" />}
-              placeholder="bruh"
+              placeholder="Password"
             />
           )}
         />
-        <input type="submit" />
-      </form>
-    </Layout.Content>
+        {errors.password && (
+          <span className="account__error">Hãy nhập password của bạn!</span>
+        )}
+        <Button
+          htmlType="submit"
+          type="primary"
+          className="d-block mx-auto w-100 mt-2 rounded-pill"
+        >
+          Đăng nhập
+        </Button>
+      </Space>
+    </form>
   );
 }
 
